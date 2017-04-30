27347af26dff7cab8386a125409d017404d747eb
Chad Austin <chad@chadaustin.me>
2017-04-30 09:08:31 -0700

    add an afl dict and fuzz the dynamic allocator

--- example/main.cpp
+++ example/main.cpp
@@ -110,7 +110,7 @@ int main(int argc, char** argv) {
     }
     fclose(file);
     
-    const sajson::document& document = sajson::parse<sajson::single_allocation>(mutable_string_view(length, buffer));
+    const sajson::document& document = sajson::parse<sajson::dynamic_allocation>(mutable_string_view(length, buffer));
     if (!success(document)) {
         return 1;
     }
--- sajson-fuzz/run.sh
+++ sajson-fuzz/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-AFL_SKIP_CPUFREQ=1 ~/src/afl-2.41b/afl-fuzz -i testcase_dir -o findings_dir -- ./sajson-fuzz @@
+AFL_SKIP_CPUFREQ=1 ~/src/afl-2.41b/afl-fuzz -i testcase_dir -o findings_dir -x json.dict -- ./sajson-fuzz @@
