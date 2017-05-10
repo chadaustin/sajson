b923f4dcaa846ed71ab6b2ed79010a352d3de21d
Aaron Eidelson <aeidelson@dropbox.com>
2017-05-09 17:32:17 -0700

    Add some basic benchmarks and support building for iOS 9.0

--- swift/sajsonTests/sajsonTests.swift
+++ swift/sajsonTests/sajsonTests.swift
@@ -1,3 +1,4 @@
+import Foundation
 import XCTest
 import sajson_swift
 
@@ -24,5 +25,61 @@ class sajsonTests: XCTestCase {
         guard case .object(let object) = docValue else { XCTFail(); return }
         XCTAssert(object.count == 1)
         guard case .some(.string("world")) = object["hello"] else { XCTFail(); return }
+    }
+
+    // MARK: Benchmarks
+
+    func test_large_json_benchmark_parse_only() {
+        let largeJsonData = createLargeTestJsonData(objectCount: 1000)
+
+        measure {
+            _ = try! parse(allocationStrategy: .single, input: largeJsonData)
+        }
+    }
+
+
+    func test_large_json_benchmark_all() {
+        let largeJsonData = createLargeTestJsonData(objectCount: 1000)
+
+        measure {
+            let doc = try! parse(allocationStrategy: .single, input: largeJsonData)
+            let swiftValue = doc.swiftValue
+
+            guard case .array(let array) = swiftValue else {
+                preconditionFailure()
+            }
+
+            // Verify that something was actually deserialized.
+            XCTAssert(array.count == 1000)
+        }
+    }
+
+    // MARK: Helpers
+
+    func createLargeTestJsonData(objectCount: Int) -> Data {
+        var largeArray = [[String: Any]]()
+        for _ in 0..<objectCount {
+            largeArray.append(createTestJsonObject())
+        }
+        return try! JSONSerialization.data(withJSONObject: largeArray)
+    }
+
+    func createTestJsonObject() -> [String: Any] {
+        var jsonDict =  [String: Any]()
+        for i in 0..<100 {
+            jsonDict["\(i)"] = randomString()
+        }
+        for i in 100..<200 {
+            jsonDict["\(i)"] = randomInt()
+        }
+        return jsonDict
+    }
+
+    private func randomString() -> String {
+        return UUID().uuidString
+    }
+
+    private func randomInt() -> Int32 {
+        return Int32(arc4random_uniform(UInt32(Int32.max)))
     }
 }
